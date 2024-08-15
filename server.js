633f46a0c7647ae47f9833207ff3c870d33520da
Jacinto Wong <jacinto.wong@cbc.ca>
2024-08-15 14:28:45 -0400

    served front-end through node server

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const app = express();
 const port = 3000;
 const wss = new WebSocket.Server({ port: 8080 });
 
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 // Endpoint to receive data from the desktop app
 app.post('/update', (req, res) => {
     const data = req.body;
-    console.log('Received data:', data);
+    console.log('Received new data');
 
     // Broadcast the data to all connected WebSocket clients
     wss.clients.forEach(client => {
